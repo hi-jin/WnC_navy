01ef8067519d6036013583d640661fd02c04a380
jeonghunYu <s_n_i_p_e_r_@naver.com>
2021-11-12 00:34:24 +0900

    update board

---
+++ board.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8">
+        <title>게시판</title>
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />
+    </head>
+    <body>
+        <div id="board_area">
+            <h1>과외 학생 모집<h1>
+            <table class="list-table">
+            <thread>
+                <tr>
+                    <th width="70">번호</th>
+                    <th width="500">제목</th>
+                    <th width="120">작성자</th>
+                    <th width="100">작성일</th>
+                    <th width="100">조회수</th>
+                </tr>
+            </thread>
+            </table>    
+            </div>
+        </div>
+    </body>
+</html>
