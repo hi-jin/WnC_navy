ca1e0c0df96396e96518bf8a0dff09a16a5700d0
jeonghunYu <s_n_i_p_e_r_@naver.com>
2021-11-12 23:08:06 +0900

    update board

--- board.php
+++ board.php
@@ -1,3 +1,4 @@
+<?php include  $_SERVER['DOCUMENT_ROOT']."root/workspace/db.php"; ?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -10,13 +11,13 @@
             <h1>과외 학생 모집<h1>
             <div id="search_area">
                 <form action="search.php">
-                <select name="searchType3" id="searchType3">
+                <select name="searchType" id="searchType">
                     <option value="all" selected="selected">전체</option>
                     <option value="title">제목</option>
                     <option value="total">모집인원</option>
                     <option value="teacher">선생님</option>
                 </select>
-                <input type="text" name="searchWord3" id="searchWord3" onclick="clickText(this.id)">
+                <input type="text" name="searchWord" id="searchWord" onclick="clickText(this.id)">
                 <input type="submit" value="검색">
                 </form>
             </div>
@@ -30,16 +31,43 @@
                     <th scope="row">등록일자</th>
                 </tr>
             </thead>
+            <?php
+             // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
+             $db = mysqli_connect("localhost", "web", "1284", "web");   
+             $sql = "select * from board order by id desc limit 0,5"; 
+                    while($board = $db->query($sql)->fetch_array(MYSQLI_ASSOC))
+                    {
+                        //title변수에 DB에서 가져온 title을 선택
+                        $title=$board["title"]; 
+                        if(strlen($title)>30)
+                        { 
+                            //title이 30을 넘어서면 ...표시
+                            $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
+                        }
+            ?>
             <tbody>
                 <tr>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    <td>
+                        <?php echo $board['id']; ?>
+                    </td>
+                    <td>
+                        <a href=""><?php echo $title;?></a>
+                    </td>
+                    <td>
+                        <?php echo $board['total']?>
+                    </td>
+                    <td>
+                        <?php echo $board['teacher']?>
+                    </td>
+                    <td>
+                        <?php echo $board['regDate']?>
+                    </td>
                 </tr>
+                <?php } ?>
             </tbody>
-            </table>    
+            </table>
+            <div id="write_btn">
+                <a href="/root/workspace/write.php"><button>글작성</button></a>
             </div>
         </div>
     </body>
